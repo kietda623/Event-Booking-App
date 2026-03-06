9566b6d9b452e1a49f5c74123188d6d4bf9a4e19
kietda623 <anhkiet240205@gmail.com>
2026-03-06 22:22:33 +0700

    add Event DTO

--- src/main/java/com/eventbooking/dto/event/EventRequest.java
+++ src/main/java/com/eventbooking/dto/event/EventRequest.java
@@ -1,4 +1,24 @@
 package com.eventbooking.dto.event;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class EventRequest {
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    @NotNull
+    private LocalDateTime eventDate;
+
+    private String location;
+
+    private Double price;
+
+    private Integer totalTickets;
 }
--- src/main/java/com/eventbooking/dto/event/EventResponse.java
+++ src/main/java/com/eventbooking/dto/event/EventResponse.java
@@ -1,4 +1,24 @@
 package com.eventbooking.dto.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class EventResponse {
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime eventDate;
+
+    private String location;
+
+    private Double price;
+
+    private Integer availableTickets;
 }
