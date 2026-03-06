b7488911ade1363276719ca33c16ea8806f08b72
kietda623 <anhkiet240205@gmail.com>
2026-03-06 22:24:52 +0700

    add Booking DTO

--- src/main/java/com/eventbooking/dto/booking/BookingRequest.java
+++ src/main/java/com/eventbooking/dto/booking/BookingRequest.java
@@ -1,4 +1,12 @@
 package com.eventbooking.dto.booking;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class BookingRequest {
+    @NotNull
+    private Long eventId;
+
+    private Integer quantity;
 }
--- src/main/java/com/eventbooking/dto/booking/BookingResponse.java
+++ src/main/java/com/eventbooking/dto/booking/BookingResponse.java
@@ -1,4 +1,23 @@
 package com.eventbooking.dto.booking;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class BookingResponse {
+    private Long bookingId;
+
+    private Long eventId;
+
+    private String eventTitle;
+
+    private Integer quantity;
+
+    private Double totalPrice;
+
+    private LocalDateTime bookingTime;
 }
