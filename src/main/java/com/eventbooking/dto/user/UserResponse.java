1c3aa64a262d22e48b38df6533f6a6f5f62e03be
kietda623 <anhkiet240205@gmail.com>
2026-03-06 22:25:53 +0700

    add User DTO

--- src/main/java/com/eventbooking/dto/user/UserResponse.java
+++ src/main/java/com/eventbooking/dto/user/UserResponse.java
@@ -1,4 +1,16 @@
 package com.eventbooking.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserResponse {
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String role;
 }
